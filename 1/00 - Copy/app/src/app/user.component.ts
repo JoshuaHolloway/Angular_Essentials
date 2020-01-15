a611153ed014d1105a3e7639a22b564196ad8b05
joshuaholloway <jhollow6@asu.edu>
2020-01-14 22:23:38 -0600

    Adding Custom Components

---
+++ 1/00 - Copy/app/src/app/user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-user',
+    template: `
+    <h1>Hello from user.component.ts</h1>
+    <h3>This is josh!</h3>
+
+    `
+})
+export class User_Component_josh { }
