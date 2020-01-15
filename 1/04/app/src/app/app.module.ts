f2f87af73ced3343f456727a581eb2d8688e7dfd
joshuaholloway <jhollow6@asu.edu>
2020-01-14 22:57:23 -0600

    Two-Way Binding

---
+++ 1/04/app/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent_josh } from './app.component';
+
+import { User_Component_josh } from './user.component';
+
+@NgModule({
+  declarations: [
+    AppComponent_josh,
+    User_Component_josh
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent_josh,]
+})
+export class AppModule { }
