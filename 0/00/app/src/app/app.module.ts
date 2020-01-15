ee60f8d811892f38daf5eca151052b37667524d2
joshuaholloway <jhollow6@asu.edu>
2020-01-14 21:36:53 -0600

    Getting Started

---
+++ 0/00/app/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent_josh } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent_josh
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent_josh]
+})
+export class AppModule { }
