d80d002fe2b08f839c6d5c40e6d800124b1f9521
joshuaholloway <jhollow6@asu.edu>
2020-01-14 22:30:37 -0600

    Outputing Content via String Interpolation

--- 1/01/app/src/app/user.component.ts
+++ 1/01/app/src/app/user.component.ts
@@ -4,8 +4,9 @@ import { Component } from '@angular/core';
     selector: 'app-user',
     template: `
     <h1>Hello from user.component.ts</h1>
-    <h3>This is josh!</h3>
-
+    <h3>This is string interpolation: {{name}} </h3>
     `
 })
-export class User_Component_josh { }
+export class User_Component_josh {
+    name = 'josh';
+}
